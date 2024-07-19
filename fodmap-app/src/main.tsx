355c62901da12bc31b8b6836d79e20ddddcbad88
jesusO <jesusdortiz18@gmail.com>
2024-07-19 13:31:16 +0200

    feat-off(sidebar)

--- fodmap-app/src/main.tsx
+++ fodmap-app/src/main.tsx
@@ -1,12 +1,12 @@
-// main.tsx o index.tsx (dependiendo de tu configuración)
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Sidebar from './components/Sidebar'; // Ajusta la ruta según tu estructura de archivos
+import Sidebar from './components/Sidebar'; 
 import './index.css';
+import { ProductApp } from './components/ProductApp';
 
 ReactDOM.render(
     <React.StrictMode>
-        <Sidebar />
+        <ProductApp />
     </React.StrictMode>,
     document.getElementById('root')
 );
