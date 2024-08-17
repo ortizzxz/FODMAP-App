0f9fad0cd9960cb4b6f5da1037b29207b9470fdb
jesusO <jesusdortiz18@gmail.com>
2024-08-17 13:21:02 +0200

    v5

--- backend/fodmap-backend/src/main/java/com/jesus/fodmapapp/fodmap_backend/entities/CorsConfig.java
+++ backend/fodmap-backend/src/main/java/com/jesus/fodmapapp/fodmap_backend/entities/CorsConfig.java
@@ -10,7 +10,12 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("https://www.tufodmap.com/", "https://fodmap-app.vercel.app/", "https://fodmap-app-ortizzxzs-projects.vercel.app/", "https://fodmap-app-git-main-ortizzxzs-projects.vercel.app/")
+            .allowedOrigins("https://www.tufodmap.com/", 
+                                        "https://fodmap-app.onrender.com/alimento",
+                                        "https://fodmap-app.onrender.com",
+                                        "https://fodmap-app.vercel.app/", 
+                                        "https://fodmap-app-ortizzxzs-projects.vercel.app/", 
+                                        "https://fodmap-app-git-main-ortizzxzs-projects.vercel.app/")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true);
--- backend/fodmap-backend/src/main/java/com/jesus/fodmapapp/fodmap_backend/entities/WebConfig.java
+++ backend/fodmap-backend/src/main/java/com/jesus/fodmapapp/fodmap_backend/entities/WebConfig.java
@@ -10,9 +10,14 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/**") // Permite todas las rutas
-            .allowedOrigins("https://www.tufodmap.com/", "https://fodmap-app.vercel.app/", "https://fodmap-app-ortizzxzs-projects.vercel.app/", "https://fodmap-app-git-main-ortizzxzs-projects.vercel.app/")
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-            .allowedHeaders("*")
-            .allowCredentials(true);
+                .allowedOrigins("https://www.tufodmap.com/",
+                        "https://fodmap-app.onrender.com/alimento",
+                        "https://fodmap-app.onrender.com",
+                        "https://fodmap-app.vercel.app/",
+                        "https://fodmap-app-ortizzxzs-projects.vercel.app/",
+                        "https://fodmap-app-git-main-ortizzxzs-projects.vercel.app/")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
