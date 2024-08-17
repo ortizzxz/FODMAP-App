b8630318a6247b78b96cccaa0d9831533c6c0886
jesusO <jesusdortiz18@gmail.com>
2024-08-17 12:01:55 +0200

    v2 cors

--- backend/fodmap-backend/src/main/java/com/jesus/fodmapapp/fodmap_backend/entities/CorsConfig.java
+++ backend/fodmap-backend/src/main/java/com/jesus/fodmapapp/fodmap_backend/entities/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("https://www.tufodmap.com/")
+            .allowedOrigins("https://www.tufodmap.com/", "https://fodmap-app.vercel.app/", "https://fodmap-app-ortizzxzs-projects.vercel.app/", "https://fodmap-app-git-main-ortizzxzs-projects.vercel.app/")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true);
