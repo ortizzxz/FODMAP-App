b7eff4ec414c595fb1ef9295955301a2994190a6
jesusO <jesusdortiz18@gmail.com>
2024-08-18 12:49:20 +0200

    encoding characthers on the backend

--- backend/fodmap-backend/src/main/java/com/jesus/fodmapapp/fodmap_backend/entities/WebConfig.java
+++ backend/fodmap-backend/src/main/java/com/jesus/fodmapapp/fodmap_backend/entities/WebConfig.java
@@ -1,8 +1,15 @@
 package com.jesus.fodmapapp.fodmap_backend.entities;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 import org.springframework.lang.NonNull; // Asegúrate de importar esta clase
 
 @Configuration
@@ -19,5 +26,15 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
+    }
+
+    @Override
+    public void addInterceptors(@NonNull InterceptorRegistry registry) {
+        registry.addInterceptor(new HandlerInterceptor() {
+            @Override
+            public void postHandle(@NonNull HttpServletRequest request,@NonNull HttpServletResponse response,@NonNull Object handler,@SuppressWarnings("null") ModelAndView modelAndView) throws Exception {
+                response.setCharacterEncoding("UTF-8");
+            }
+        });
     }
 }
