c51b3f6dd3653af90c1d2c88cf9e91a371079546
jesusO <jesusdortiz18@gmail.com>
2024-08-20 15:47:16 +0200

    chaging front route link to the back
    from /alimento to /alimentoes

--- fodmap-app/src/services/foodService.tsx
+++ fodmap-app/src/services/foodService.tsx
@@ -1,7 +1,6 @@
 import axios, { AxiosResponse } from "axios";
 
-const URL = 'https://fodmap-app.onrender.com/alimento';
-const localUrl = 'http://localhost:8080/alimentoes'; // this is for local testing 
+const URL = 'https://fodmap-app.onrender.com/alimentoes'
 
 // vamos a definir una interfaz para el tipo de dato que espero recibir desde el backend 
 interface Alimento {
