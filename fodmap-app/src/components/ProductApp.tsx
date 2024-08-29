5d26ee48d8558987b8b8de6f55e35d715fac19b9
jesusO <jesusdortiz18@gmail.com>
2024-08-29 10:58:21 +0200

    fix(color of filter svg)

--- fodmap-app/src/components/ProductApp.tsx
+++ fodmap-app/src/components/ProductApp.tsx
@@ -119,7 +119,7 @@ export const ProductApp: React.FC = () => {
                                 onClick={toggleFilters}
                             >
                                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M22 3H2L10 12.46V19L14 21V12.46L22 3Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                                    <path d="M22 3H2L10 12.46V19L14 21V12.46L22 3Z" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                                 </svg>
                             </button>
                         </div>
