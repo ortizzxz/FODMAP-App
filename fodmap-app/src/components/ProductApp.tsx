d70b93305aef30eee9f3fdeeccbfa31c7febaf2a
jesusO <jesusdortiz18@gmail.com>
2024-09-04 13:30:50 +0200

    fix(aria-label to button filter accesibility improvement)

--- fodmap-app/src/components/ProductApp.tsx
+++ fodmap-app/src/components/ProductApp.tsx
@@ -117,6 +117,7 @@ export const ProductApp: React.FC = () => {
                                 flex items-center justify-center relative overflow-hidden
                                 active:translate-y-[2px] active:shadow-inner'
                                 onClick={toggleFilters}
+                                aria-label='Filtrar resultados'
                             >
                                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M22 3H2L10 12.46V19L14 21V12.46L22 3Z" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
