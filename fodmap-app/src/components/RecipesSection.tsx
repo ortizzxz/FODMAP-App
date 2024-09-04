577c6f4fa568296b1f15b847f6cfb8acd5fbfbba
jesusO <jesusdortiz18@gmail.com>
2024-09-04 12:35:41 +0200

    fix(gap between recipes legend)

--- fodmap-app/src/components/RecipesSection.tsx
+++ fodmap-app/src/components/RecipesSection.tsx
@@ -14,13 +14,13 @@ text-center text-xl flex flex-col  border-main border-1*/
 
     return (
         <div className="h-full w-full overflow-auto mt-[2%]  mr-1 scroll-container"> 
-            <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-0 justify-items-center'>
+            <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-6 justify-items-center'>
                 {recipes.map((recipe) => (
-                    <div key={recipe.id} className="bg-white rounded-lg shadow-md overflow-hidden flex flex-col h-80 w-full max-w-sm">
+                    <div key={recipe.id} className="border-1 border-main rounded-lg shadow-md overflow-hidden flex flex-col h-80 w-full max-w-sm">
                         <div className="flex-grow">
                             <img src="src/assets/defaultImage.jpg" alt="Imagen receta" className="h-full w-full object-cover"/>
                         </div>
-                        <h1 className="text-lg text-center font-semibold">{recipe.title}</h1>
+                        <h1 className="text-xl p-2 text-center font-semibold">{recipe.title}</h1>
                     </div>
                 ))}
             </div>
